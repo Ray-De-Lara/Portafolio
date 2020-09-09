2d888677cbfb629ea81d71b112d8a6c9abda0c62
Ray-De-Lara <raydelara99@gmail.com>
2020-09-09 14:36:53 -0500

    index de mi pagina

---
+++ js/index.js
@@ -0,0 +1,45 @@
+//typewriter effect for HOLA text
+
+var text = document.getElementById("hola");
+var text1 = document.getElementById("saludo");
+
+var str = text.innerHTML;
+var str1 = text1.innerHTML;
+
+
+
+text.innerHTML = "";
+text1.innerHTML = "";
+
+
+var speed = 200;
+
+var i = 0;
+var j = 0;
+
+
+function typewriter(){
+    if(i < str.length){
+        text.innerHTML += str.charAt(i);
+        i++;
+        setTimeout(typewriter, speed);   
+    }
+    else{
+        function typewriter1(){
+            if(j < str1.length){
+                
+                text1.innerHTML += str1.charAt(j);
+                j++;
+                setTimeout(typewriter1, speed);
+            }
+        }
+        
+        setTimeout(typewriter1,speed);
+    }
+}
+
+setTimeout(typewriter,speed);
+
+
+//type writer effect for SALUDO text
+
