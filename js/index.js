388d56e5dabf428e0a8d3af75994943051dd2fea
Ray-De-Lara <raydelara99@gmail.com>
2020-10-16 11:15:50 -0500

    responsive version prueba ya esta bonita

--- js/index.js
+++ js/index.js
@@ -1,3 +1,23 @@
+//View menu
+jQuery('document').ready(function($){
+
+    var menuBtn = $('.menu-icon'),
+        menu = $('.navigation ul');
+
+    menuBtn.click(function() {
+
+
+        if(menu.hasClass('show')){
+            menu.removeClass('show');
+        }
+        else{
+            menu.addClass('show');
+        }
+        
+    });
+
+});
+
 //typewriter effect for HOLA text
 
 var text = document.getElementById("hola");
